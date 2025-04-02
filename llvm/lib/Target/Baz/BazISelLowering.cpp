78fa8fa7e681584dc2761e7dfcbc28ed1743c998
as3rg <sergeev0xEF@gmail.com>
2025-04-02 16:21:12 +0300

    15. Add RegisterInfo to TargetLowering

--- llvm/lib/Target/Baz/BazISelLowering.cpp
+++ llvm/lib/Target/Baz/BazISelLowering.cpp
@@ -39,7 +39,29 @@ BazTargetLowering::BazTargetLowering(const TargetMachine &TM,
     : TargetLowering(TM), STI(STI) {
   BAZ_DUMP_RED
   addRegisterClass(MVT::i32, &Baz::GPRRegClass);
+  computeRegisterProperties(STI.getRegisterInfo());
+
+  setStackPointerRegisterToSaveRestore(Baz::R1);
+
+  // setSchedulingPreference(Sched::Source);
+
+  for (unsigned Opc = 0; Opc < ISD::BUILTIN_OP_END; ++Opc)
+    setOperationAction(Opc, MVT::i32, Expand);
+
+  setOperationAction(ISD::ADD, MVT::i32, Legal);
+  setOperationAction(ISD::MUL, MVT::i32, Legal);
+  // ...
+  setOperationAction(ISD::LOAD, MVT::i32, Legal);
+  setOperationAction(ISD::STORE, MVT::i32, Legal);
+
+  setOperationAction(ISD::Constant, MVT::i32, Legal);
+  setOperationAction(ISD::UNDEF, MVT::i32, Legal);
+
+  setOperationAction(ISD::BR_CC, MVT::i32, Custom);
+
+  setOperationAction(ISD::FRAMEADDR, MVT::i32, Legal);
 }
+
 
 const char *BazTargetLowering::getTargetNodeName(unsigned Opcode) const {
   BAZ_DUMP_RED
