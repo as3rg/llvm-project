96c1a3b72792959e0b893369a9b3c44a41d75f26
as3rg <sergeev0xEF@gmail.com>
2025-04-02 16:08:36 +0300

    13. Calling Convention & MachineFunction & Lowering Function

--- llvm/lib/Target/Baz/BazISelLowering.h
+++ llvm/lib/Target/Baz/BazISelLowering.h
@@ -12,6 +12,7 @@ class BazTargetMachine;
 namespace BazISD {
 
 enum NodeType : unsigned {
+  // Start the numbering where the builtin ops and target ops leave off.
   FIRST_NUMBER = ISD::BUILTIN_OP_END,
   RET,
   CALL,
@@ -22,15 +23,45 @@ enum NodeType : unsigned {
 
 class BazTargetLowering : public TargetLowering {
 public:
-    explicit BazTargetLowering(const TargetMachine &TM, const BazSubtarget &STI);
+  explicit BazTargetLowering(const TargetMachine &TM, const BazSubtarget &STI);
 
-    /// This method returns the name of a target specific DAG node.
-    const char *getTargetNodeName(unsigned Opcode) const override;
+  /// This method returns the name of a target specific DAG node.
+  const char *getTargetNodeName(unsigned Opcode) const override;
 
-    BazSubtarget const &getSubtarget() const { return STI; }
+  /// Return true if the addressing mode represented by AM is legal for this
+  /// target, for a load/store of the specified type.
+  bool isLegalAddressingMode(const DataLayout &DL, const AddrMode &AM, Type *Ty,
+                             unsigned AS,
+                             Instruction *I = nullptr) const override;
+
+  BazSubtarget const &getSubtarget() const { return STI; }
 
 private:
-    const BazSubtarget &STI;
+  const BazSubtarget &STI;
+
+  void ReplaceNodeResults(SDNode *N, SmallVectorImpl<SDValue> &Results,
+                          SelectionDAG &DAG) const override;
+
+  SDValue PerformDAGCombine(SDNode *N, DAGCombinerInfo &DCI) const override;
+
+  SDValue LowerFormalArguments(SDValue Chain, CallingConv::ID CallConv,
+                               bool IsVarArg,
+                               const SmallVectorImpl<ISD::InputArg> &Ins,
+                               const SDLoc &DL, SelectionDAG &DAG,
+                               SmallVectorImpl<SDValue> &InVals) const override;
+
+  SDValue LowerCall(TargetLowering::CallLoweringInfo &CLI,
+                    SmallVectorImpl<SDValue> &InVals) const override;
+
+  SDValue LowerReturn(SDValue Chain, CallingConv::ID CallConv, bool IsVarArg,
+                      const SmallVectorImpl<ISD::OutputArg> &Outs,
+                      const SmallVectorImpl<SDValue> &OutVals, const SDLoc &DL,
+                      SelectionDAG &DAG) const override;
+
+  bool CanLowerReturn(CallingConv::ID CallConv, MachineFunction &MF,
+                      bool IsVarArg,
+                      const SmallVectorImpl<ISD::OutputArg> &ArgsFlags,
+                      LLVMContext &Context, const Type *RetTy) const override;
 };
 
 } // end namespace llvm
